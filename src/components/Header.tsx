d5af5dcc73e3122a1730183210eebd6f6ead1a8e
Joao Guimaraes <psjoaovictor@gmail.com>
2022-05-28 15:27:24 -0300

    refactor: :recycle: ending header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { Flex, Icon, Input, Text } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
-    <Flex 
-        as="header" 
-        w="100%" 
-        maxWidth={1400} 
-        h="20" 
-        mx="auto" 
-        mt="4" 
-        px="6" 
-        align="center"
-    >
+    <Flex as="header" w="100%" maxWidth={1400} h="20" mx="auto" mt="4" px="6" align="center">
       <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
         Dashgo
         <Text as="span" ml="1" color="pink.500">
@@ -42,6 +33,29 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.500"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Joao Guimaraes</Text>
+            <Text color="gray.300">psjoaovictor@gmail.com</Text>
+          </Box>
+          <Avatar size="md" name="Joao Guimaraes" src="https://github.com/Joaopsguimaraes.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
