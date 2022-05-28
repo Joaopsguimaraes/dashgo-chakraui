954ae4f21a24517d8a45268671db21859506e2e0
Joao Guimaraes <psjoaovictor@gmail.com>
2022-05-28 12:48:00 -0300

    feat: :sparkles: config chakra ui and validate import components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { Flex, Heading } from "@chakra-ui/react";
+
+function Home() {
+  return (
+    <Flex w='100vw' h='100vh' alignItems='center' justifyContent='center'>
+      <Heading>Dashgo</Heading>
+    </Flex>
+  );
+}
+
+export default Home;
